2d023b31fa352444a87b59b676bfac4b714a73d9
Zachary Durr <zdurr22@gmail.com>
2020-10-16 18:20:31 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,10 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
+    if person == "Freddie Mercury"
+      contact_details_hash.each do |attribute, data|
+      end
+    end
     binding.pry
   end
 end
