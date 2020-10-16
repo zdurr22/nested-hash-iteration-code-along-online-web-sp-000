ba80842fb26912f69777212ccefe6a99731a8aaf
Zachary Durr <zdurr22@gmail.com>
2020-10-16 18:26:26 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,14 +13,14 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  def remove_strawberry(contacts)
-    contacts.each do |person, contact_details_hash|
-      if person == "Freddy Mercury"
-        contact_details_hash.each do |attribute, data|
-          if attribute == :favorite_ice_cream_flavors
-            data.delete_if {|ice_cream| ice_cream == "strawberry"}
-          end
+def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddie Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
         end
       end
     end
   end
+end
